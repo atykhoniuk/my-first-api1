c7c5df4c6ba00bb7822bbb8f5cd7b120162b3b1b
Aleksandr <tihonyuk1999@gmail.com>
2019-12-30 17:58:09 +0200

    Work with Database
    Make connection with server.Creation database. Creation table User.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+4
+.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+M
+config/packages/doctrine.yaml,8\4\8407b4a9f249ef233d425138c0e8540c756c614a
---
+++ src/Entity/User.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fio;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $phone_number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthdaydate;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $sex;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFio(): ?string
+    {
+        return $this->fio;
+    }
+
+    public function setFio(string $fio): self
+    {
+        $this->fio = $fio;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(string $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getBirthdaydate(): ?\DateTimeInterface
+    {
+        return $this->birthdaydate;
+    }
+
+    public function setBirthdaydate(\DateTimeInterface $birthdaydate): self
+    {
+        $this->birthdaydate = $birthdaydate;
+
+        return $this;
+    }
+
+    public function getSex(): ?string
+    {
+        return $this->sex;
+    }
+
+    public function setSex(?string $sex): self
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+}
